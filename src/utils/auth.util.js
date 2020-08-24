3a967713dad457017dc68aa96a7efcd1182b4d00
CL-Sudo <laucl0001@outlook.com>
2020-08-24 12:17:24 +0800

    enhanced auth

--- src/utils/auth.util.js
+++ src/utils/auth.util.js
@@ -1,6 +1,10 @@
 import jwt from 'jsonwebtoken';
 import randtoken from 'rand-token';
 import * as Config from '@configs';
+import { v4 as uuidv4 } from 'uuid';
+import R from 'ramda';
+import { Users } from '@models';
+import { Op } from 'sequelize';
 
 export const generateRefreshToken = () => randtoken.uid(255);
 
@@ -41,3 +45,41 @@ export const generateResetToken = async email =>
       return reject(e);
     }
   });
+
+export const generateUsername = async (proposedName, generatedName) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const normalizedUsername = R.replace(/ /g, '.')(R.toLower(proposedName));
+      const user = await Users.findOne({
+        raw: true,
+        where: {
+          username: {
+            [Op.like]: `%${generatedName || normalizedUsername}%`
+          }
+        }
+      });
+
+      if (user) {
+        const generated = `${normalizedUsername}.${uuidv4()}`;
+        return resolve(await generateUsername(normalizedUsername, generated));
+      }
+
+      return resolve(generatedName || normalizedUsername);
+    } catch (e) {
+      return reject(e);
+    }
+  });
+
+export const parseFirstNameLastName = fullName => {
+  const splited = R.split(' ')(R.trim(fullName));
+  if (R.length(splited) > 1) {
+    return {
+      firstName: splited[0],
+      lastName: R.last(splited)
+    };
+  }
+  return {
+    firstName: null,
+    lastName: splited[0]
+  };
+};
