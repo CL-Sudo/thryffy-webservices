0609827c1c76b4e6eb33b8e21cb6fef889fa62a3
CL-Sudo <laucl0001@outlook.com>
2020-09-15 12:25:44 +0800

    added get categories id api

---
+++ src/controllers/categories.controller.js
@@ -0,0 +1,55 @@
+import { Categories } from '@models';
+import { Op } from 'sequelize';
+import { requestValidator } from '@validators';
+import R from 'ramda';
+
+export const list = async (req, res, next) => {
+  try {
+    const { parent, childId, limit, offset } = req.query;
+    requestValidator(req);
+
+    const getChildren = async () => {
+      try {
+        const parentObject = await Categories.findOne({
+          where: {
+            title: {
+              [Op.like]: `%${parent}%`
+            }
+          }
+        });
+
+        const children = await Categories.findAndCountAll({
+          where: { parentId: parentObject.id },
+          limit,
+          offset
+        });
+        return Promise.resolve(children);
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    };
+
+    const getCategories = async children => {
+      try {
+        if (R.isNil(childId)) return Promise.resolve(children);
+        const childArr = await Categories.findAndCountAll({
+          where: { parentId: childId },
+          limit,
+          offset
+        });
+        return Promise.resolve(childArr);
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    };
+
+    const result = await R.pipeP(getChildren, getCategories)();
+
+    return res.status(200).json({
+      message: 'success',
+      payload: result
+    });
+  } catch (e) {
+    return next(e);
+  }
+};
---
+++ src/validators/categories.validator.js
@@ -0,0 +1,54 @@
+import { check } from 'express-validator/check';
+import R from 'ramda';
+import { Categories } from '@models';
+
+const identityOrDefault = (param, def) => {
+  if (R.length(param) === 0) return def;
+  return R.toUpper(param);
+};
+
+export const listValidator = [
+  check('parent')
+    .exists()
+    .withMessage('Required')
+    .custom(parent => {
+      const isMen = R.toUpper(parent) === 'MEN';
+      const isWomen = R.toUpper(parent) === 'WOMEN';
+      const isKids = R.toUpper(parent) === 'KIDS';
+
+      if (!isMen && !isWomen && !isKids) {
+        throw new Error('Invalid');
+      }
+
+      return Promise.resolve();
+    })
+    .customSanitizer(parent => identityOrDefault(parent, 'WOMEN')),
+  check('childId')
+    .exists()
+    .withMessage('Required')
+    .custom(async childId => {
+      if (childId) {
+        const result = await Categories.findOne({
+          where: { id: childId }
+        });
+        if (R.isNil(result)) throw new Error('Invalid childId given');
+      }
+
+      return Promise.resolve();
+    })
+    .customSanitizer(child => {
+      const isString = R.type(child) === 'String';
+      if (isString && R.length(child) === 0) {
+        return null;
+      }
+      return child;
+    }),
+  check('limit')
+    .exists()
+    .withMessage('required')
+    .customSanitizer(limit => (limit ? Number(limit) : null)),
+  check('offset')
+    .exists()
+    .withMessage('required')
+    .customSanitizer(offset => (offset ? Number(offset) : null))
+];
