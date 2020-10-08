77d4a335c760177d7abaf278d029035326ae7313
CL-Sudo <laucl0001@outlook.com>
2020-10-08 16:44:10 +0800

    added default column to sizes table

--- sequelize/seeders/20201005152536-create_category_size_seeder.js
+++ sequelize/seeders/20201005152536-create_category_size_seeder.js
@@ -37,87 +37,87 @@ module.exports = {
           const root = await c.getRoot();
           if (isWomen(root.title)) {
             if (
-              c.title !== 'Women' ||
-              c.title === 'Lingerie' ||
-              c.title === 'Underwears' ||
-              c.title === 'Tops & T-Shirts' ||
-              c.title === 'Shirts' ||
-              c.title === 'Upper Active Wears' ||
-              c.title === 'Night Wears' ||
-              c.title === 'Skirts' ||
-              c.title === 'Dresses' ||
-              c.title === 'Jumpsuits & Rompers' ||
-              c.title === 'Swim Suits' ||
-              c.title === 'Special Costumes & Outfits' ||
-              c.title === 'Sweaters' ||
-              c.title === 'Winter Jackets' ||
-              c.title === 'Jackets' ||
-              c.title === 'Coats' ||
-              c.title === 'Suits' ||
-              c.title === 'Blazers'
+              c.title !== 'Women' &&
+              (c.title === 'Lingerie' ||
+                c.title === 'Underwears' ||
+                c.title === 'Tops & T-Shirts' ||
+                c.title === 'Shirts' ||
+                c.title === 'Upper Active Wears' ||
+                c.title === 'Night Wears' ||
+                c.title === 'Skirts' ||
+                c.title === 'Dresses' ||
+                c.title === 'Jumpsuits & Rompers' ||
+                c.title === 'Swim Suits' ||
+                c.title === 'Special Costumes & Outfits' ||
+                c.title === 'Sweaters' ||
+                c.title === 'Winter Jackets' ||
+                c.title === 'Jackets' ||
+                c.title === 'Coats' ||
+                c.title === 'Suits' ||
+                c.title === 'Blazers')
             ) {
               await seedSizes(Type.WOMEN_CLOTHING, c.id);
             } else if (
-              c.title !== 'Women' ||
-              c.title === 'Tights & Pantyhoses' ||
-              c.title === 'Leggings' ||
-              c.title === 'Shorts & Capri Pants' ||
-              c.title === 'Lower Active Wears'
+              c.title !== 'Women' &&
+              (c.title === 'Tights & Pantyhoses' ||
+                c.title === 'Leggings' ||
+                c.title === 'Shorts & Capri Pants' ||
+                c.title === 'Lower Active Wears')
             ) {
               await seedSizes(Type.WOMEN_PANT, c.id);
-            } else if (c.title === 'Socks' || c.title === 'Shoes' || c.title !== 'Women') {
+            } else if ((c.title === 'Socks' || c.title === 'Shoes') && c.title !== 'Women') {
               await seedSizes(Type.WOMEN_SHOES, c.id);
             }
           } else if (isMen(root.title)) {
             if (
-              c.title !== 'Men' ||
-              c.title === 'Underwears' ||
-              c.title === 'Tops & T-Shirts' ||
-              c.title === 'Shirts' ||
-              c.title === 'Upper Active Wears' ||
-              c.title === 'Pajamas Tops' ||
-              c.title === 'Active Wears' ||
-              c.title === 'Swim Wears' ||
-              c.title === 'Sweaters' ||
-              c.title === 'Winter Jackets' ||
-              c.title === 'Jackets' ||
-              c.title === 'Coats' ||
-              c.title === 'Suits' ||
-              c.title === 'Blazers'
+              c.title !== 'Men' &&
+              (c.title === 'Underwears' ||
+                c.title === 'Tops & T-Shirts' ||
+                c.title === 'Shirts' ||
+                c.title === 'Upper Active Wears' ||
+                c.title === 'Pajamas Tops' ||
+                c.title === 'Active Wears' ||
+                c.title === 'Swim Wears' ||
+                c.title === 'Sweaters' ||
+                c.title === 'Winter Jackets' ||
+                c.title === 'Jackets' ||
+                c.title === 'Coats' ||
+                c.title === 'Suits' ||
+                c.title === 'Blazers')
             ) {
               await seedSizes(Type.MEN_CLOTHING, c.id);
             } else if (
-              c.title !== 'Men' ||
-              c.title === 'Pajamas Pants' ||
-              c.title === 'Pants' ||
-              c.title === 'Jeans' ||
-              c.title === 'Lower Active Wears' ||
-              c.title === 'Swim Trunks'
+              c.title !== 'Men' &&
+              (c.title === 'Pajamas Pants' ||
+                c.title === 'Pants' ||
+                c.title === 'Jeans' ||
+                c.title === 'Lower Active Wears' ||
+                c.title === 'Swim Trunks')
             ) {
               await seedSizes(Type.MEN_PANT, c.id);
-            } else if (c.title === 'Socks' || c.title === 'Shoes' || c.title !== 'Men') {
+            } else if ((c.title === 'Socks' || c.title === 'Shoes') && c.title !== 'Men') {
               await seedSizes(Type.MEN_SHOES, c.id);
             }
           } else if (isKid(root.title)) {
             if (
-              c.title !== 'Kids' ||
-              c.title === 'Baby Clothing' ||
-              c.title === 'Bottoms' ||
-              c.title === 'Dresses' ||
-              c.title === 'One Pieces' ||
-              c.title === 'Pajamas' ||
-              c.title === 'Shirts & Tops' ||
-              c.title === 'Swims' ||
-              c.title === 'Costumes' ||
-              c.title === 'Jackets' ||
-              c.title === 'Coats' ||
-              c.title === 'Suits' ||
-              c.title === 'Blazers' ||
-              c.title === 'Special Costumes & Outfits'
+              c.title !== 'Kids' &&
+              (c.title === 'Baby Clothing' ||
+                c.title === 'Bottoms' ||
+                c.title === 'Dresses' ||
+                c.title === 'One Pieces' ||
+                c.title === 'Pajamas' ||
+                c.title === 'Shirts & Tops' ||
+                c.title === 'Swims' ||
+                c.title === 'Costumes' ||
+                c.title === 'Jackets' ||
+                c.title === 'Coats' ||
+                c.title === 'Suits' ||
+                c.title === 'Blazers' ||
+                c.title === 'Special Costumes & Outfits')
             ) {
               await seedSizes(Type.KID_CLOTHING, c.id);
-            } else if (c.title === 'Shoes' || c.title !== 'Kids') {
-              await seedSizes(Type.KID_SHOES);
+            } else if (c.title === 'Shoes' && c.title !== 'Kids') {
+              await seedSizes(Type.KID_SHOES, c.id);
             }
           }
         })(categories)
