c013b71fd55d514c79b24d716dfc135d6dd72860
CL-Sudo <laucl0001@outlook.com>
2020-10-14 16:05:46 +0800

    modify get size api

--- src/controllers/test.controller.js
+++ src/controllers/test.controller.js
@@ -1,10 +1,22 @@
-import { getOneProductShippingFee } from '@services/product.service';
+import { Categories } from '@models';
 
 export const test = async (req, res, next) => {
   try {
-    const fee = await getOneProductShippingFee(req.query.categoryId, req.query.sizeId);
+    const { q } = req.query;
+
+    const sizes = await Categories.findAll({
+      where: { id: q },
+      include: [
+        {
+          model: Categories,
+          as: 'subCategories'
+        }
+      ]
+    });
+
     return res.status(200).json({
-      message: 'success'
+      message: 'success',
+      payload: sizes
     });
   } catch (e) {
     return next(e);
