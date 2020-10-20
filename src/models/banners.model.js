82633a3627d9fc398454c4116953a3d4b3ee5d87
CL-Sudo <laucl0001@outlook.com>
2020-10-20 10:15:20 +0800

    added banner module

---
+++ src/models/banners.model.js
@@ -0,0 +1,45 @@
+import { SequelizeConnector, Sequelize } from '@configs/sequelize-connector.config';
+import { addScopesByAllFields, search } from '@utils/sequelize-scopes.util';
+import { AT_RECORDER, BY_RECORDER, primaryKey } from '@constants/sequelize.constant';
+import { parseParanoidToIncludes } from '@utils/sequelize-hooks.util';
+import { deleteObjectFromS3 } from '@tools/s3';
+import { parsePathForDeleting } from '@utils/s3.util';
+
+const Banners = SequelizeConnector.define(
+  'Banners',
+  {
+    id: primaryKey,
+    path: {
+      type: Sequelize.STRING
+    },
+    index: {
+      type: Sequelize.INTEGER
+    },
+    ...AT_RECORDER,
+    ...BY_RECORDER
+  },
+  {
+    tableName: 'banners',
+    underscored: false,
+    scopes: {
+      search: params => search(Banners, params, [])
+    },
+    hooks: {
+      beforeFind: query => {
+        parseParanoidToIncludes(query);
+      },
+      beforeDestroy: async banner => {
+        try {
+          await deleteObjectFromS3(parsePathForDeleting(banner.path));
+        } catch (e) {
+          throw e;
+        }
+      }
+    }
+  }
+);
+
+addScopesByAllFields(Banners, []);
+
+export { Banners };
+export default Banners;
