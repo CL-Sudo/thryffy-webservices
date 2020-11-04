524af3cc34768d1db6ad593cb37899b7d9630955
CL-Sudo <laucl0001@outlook.com>
2020-11-04 12:05:58 +0800

    integrated AWS key, modification of the .env file need

--- src/controllers/test.controller.js
+++ src/controllers/test.controller.js
@@ -1,8 +1,12 @@
 import { generateOrderNumber } from '@utils/sales_orders.util';
+import { sendMail } from '@tools/sendgrid';
+import SENDGRID from '@constants/sendgrid.constant';
 
 export const test = async (req, res, next) => {
   try {
     const { q } = req.query;
+
+    await sendMail('elgoogym0001@gmail.com', 'Ching Lung', 'Lau', {}, SENDGRID.TEST);
 
     return res.status(404).json({
       message: 'not found',
