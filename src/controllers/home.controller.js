ea6b8164499c4991a08769f9b7e96031d99ef461
CL-Sudo <laucl0001@outlook.com>
2020-12-09 13:35:11 +0800

    fixed bugs

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,5 +1,6 @@
 import R from 'ramda';
 import { Preferences, Products, Banners, FeatureItems } from '@models';
+import { paginate } from '@utils';
 
 export const getBannersList = async (req, res, next) => {
   try {
@@ -53,17 +54,33 @@ export const getCuratedList = async (req, res, next) => {
 
     const preferenceIds = getPreferableObjList(preferences);
 
-    const products = await Products.scope('default').findAndCountAll({
-      where: {
-        categoryId: preferenceIds.category,
-        brandId: preferenceIds.brand,
-        conditionId: preferenceIds.condition
-      },
-      limit: Number(limit) || null,
-      offset: Number(offset) || null
+    const assignCategory = R.isEmpty(preferenceIds.category)
+      ? R.identity
+      : R.assoc('categoryId', preferenceIds.category);
+
+    const assignCondition = R.isEmpty(preferenceIds.condition)
+      ? R.identity
+      : R.assoc('conditionId', preferenceIds.condition);
+
+    const assignBrand = R.isEmpty(preferenceIds.brand)
+      ? R.identity
+      : R.assoc('brandId', preferenceIds.brand);
+
+    const where = R.pipe(assignBrand, assignCategory, assignCondition)({});
+
+    console.log('where', where);
+
+    const products = await Products.scope('default').findAll({
+      where
     });
 
-    return res.status(200).json({ message: 'success', payload: products });
+    return res.status(200).json({
+      message: 'success',
+      payload: {
+        count: products.length,
+        rows: paginate(limit)(offset)(products)
+      }
+    });
   } catch (e) {
     return next(e);
   }
