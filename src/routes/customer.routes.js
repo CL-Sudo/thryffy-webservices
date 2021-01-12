7c44f0d6352db723788acafe16bffe68528d8a03
Benson Choong <64839865+bensonchoong97@users.noreply.github.com>
2021-01-12 12:10:31 +0800

    Fixed customer api

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,7 +1,7 @@
 import Router from 'express';
 import { crud } from '@utils/controller-crud.util';
 import { Users } from '@models';
-import { list } from '@controllers/Admin/customers.controller';
+import { list, getCustomerProductRequest } from '@controllers/Admin/customers.controller';
 
 const controller = crud(Users);
 
@@ -13,6 +13,8 @@ router
   .route('/:id')
   .get(controller.readOne)
   .delete(controller.destroy);
+
+router.route('/:id/products').get(getCustomerProductRequest);
 
 router.route('/activate/:id').patch(controller.activate);
 router.route('/deactivate/:id').patch(controller.deactivate);
