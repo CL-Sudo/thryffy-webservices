d3087411ea7d00bf43ebae3bc6adfdf8c7a4f99f
Benson Choong <64839865+bensonchoong97@users.noreply.github.com>
2021-01-12 12:10:31 +0800

    Fixed customer api

--- src/controllers/Admin/customers.controller.js
+++ src/controllers/Admin/customers.controller.js
@@ -1,36 +1,43 @@
 import { Users, Products } from '@models';
 import { Op } from 'sequelize';
 import { paginate } from '@utils';
+import { getScopes, getLimitOffset } from '@utils/express.util';
 
 export const list = async (req, res, next) => {
   try {
-    const { keyword, limit, offset } = req.query;
+    const scopes = getScopes(Users)(req);
+    const { limit, offset } = getLimitOffset(req);
 
-    const users = await Users.findAll({
-      include: [{ model: Products, as: 'products' }],
-      where: {
-        [Op.or]: [
-          {
-            username: {
-              [Op.like]: `%${keyword}%`
-            }
-          },
-          {
-            fullName: {
-              [Op.like]: `%${keyword}%`
-            }
-          }
-        ]
-      }
+    const users = await Users.scope(scopes).findAndCountAll({
+      limit,
+      offset,
+      // include: [{ model: Products, as: 'products' }],
+      raw: true,
+      hooks: false
     });
 
-    return res.status(200).json({
-      message: 'success',
-      payload: {
-        count: users.length,
-        rows: paginate(limit)(offset)(users)
-      }
+    return res
+      .status(200)
+      .json({ message: 'success', payload: { rows: users.rows, count: users.count } });
+  } catch (e) {
+    return next(e);
+  }
+};
+
+export const getCustomerProductRequest = async (req, res, next) => {
+  try {
+    const { id: customerId } = req.params;
+    const scopes = getScopes(Products)(req);
+    const { limit, offset } = getLimitOffset(req);
+
+    const products = await Products.scope(scopes).findAndCountAll({
+      where: { userId: customerId },
+      limit,
+      offset,
+      raw: true
     });
+
+    return res.status(200).json({ message: 'Success', payload: products });
   } catch (e) {
     return next(e);
   }
