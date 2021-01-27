61ea1426320038bb0dcf801a0d67dc239d3a8b37
Benson Choong <64839865+bensonchoong97@users.noreply.github.com>
2021-01-27 09:33:10 +0800

    Added get product list

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,7 +2,12 @@ import Router from 'express';
 import { crud } from '@utils/controller-crud.util';
 import { Products } from '@models';
 import { addProduct, updateProduct } from '@controllers/seller.controller';
-import { managePublication } from '@controllers/Admin/products.controller';
+import {
+  managePublication,
+  verifyProductRequest,
+  unVerifyProductRequest,
+  getProductListRequest
+} from '@controllers/Admin/products.controller';
 
 const controller = crud(Products);
 
@@ -10,7 +15,7 @@ const router = new Router();
 
 router
   .route('/')
-  .get(controller.read)
+  .get(getProductListRequest)
   .post(addProduct);
 
 router
@@ -20,5 +25,8 @@ router
   .put(updateProduct);
 
 router.route('/:productId/publications').patch(managePublication);
+
+router.route('/:productId/verify').patch(verifyProductRequest);
+router.route('/:productId/un-verify').patch(unVerifyProductRequest);
 
 export default router;
