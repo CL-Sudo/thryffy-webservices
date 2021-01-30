6fda03803f587826b1b540e2f47ed577f7aa4523
CL-Sudo <laucl0001@outlook.com>
2021-01-30 16:55:25 +0800

    task on 30-01-21

--- src/validators/subscription.validator.js
+++ src/validators/subscription.validator.js
@@ -1,22 +1,14 @@
 import { check } from 'express-validator/check';
-import { Subscriptions, Packages } from '@models';
+import { Packages } from '@models';
 
 export const subscribeValidator = [
   check('packageId')
     .exists()
     .isLength({ min: 1 })
-    .custom(async (packageId, { req }) => {
-      const { id } = req.user;
+    .custom(async packageId => {
       const pck = await Packages.findOne({ where: { id: packageId } });
 
       if (!pck) throw new Error('Invalid packageId given.');
-
-      const currentSubscription = await Subscriptions.findOne({ where: { userId: id } });
-
-      if (currentSubscription && currentSubscription.expiryDate > new Date()) {
-        throw new Error('You subscription is not expired yet.');
-      }
-
       return Promise.resolve();
     })
 ];
