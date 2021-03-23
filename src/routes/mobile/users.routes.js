8826179b420904be2b330f072ddf4270f631aa77
CL-Sudo <laucl0001@outlook.com>
2021-03-23 12:58:18 +0800

    changed search people route

--- src/routes/mobile/users.routes.js
+++ src/routes/mobile/users.routes.js
@@ -1,9 +1,9 @@
 import Router from 'express';
-import { search } from '@controllers/users.controller';
-import { searchValidator } from '@validators/users.validator';
+// import { search } from '@controllers/users.controller';
+// import { searchValidator } from '@validators/users.validator';
 
 const router = new Router();
 
-router.get('/search', searchValidator, search);
+// router.get('/search', searchValidator, search);
 
 export default router;
