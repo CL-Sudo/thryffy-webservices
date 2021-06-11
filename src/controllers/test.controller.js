537f3b5f065e7453a9e4b54b99899d067b60e56e
CL-Sudo <laucl0001@outlook.com>
2021-06-11 09:48:30 +0800

    fixed delete customer error

--- src/controllers/test.controller.js
+++ src/controllers/test.controller.js
@@ -1,6 +1,11 @@
+import { Users } from '@models';
+
 export const test = async (req, res, next) => {
   try {
     // const { q } = req.query;
+
+    const user = await Users.findOne({ where: { id: 57 } });
+    console.log(`user`, user);
 
     return res.status(200).json({
       message: 'not found'
