dd2ef4a0641429630e181feeb20cf07b8b639a37
CL-Sudo <laucl0001@outlook.com>
2021-08-09 14:43:15 +0800

    added share profile

--- src/controllers/share.controller.js
+++ src/controllers/share.controller.js
@@ -32,3 +32,35 @@ export const shareProduct = async (req, res, next) => {
     return next(e);
   }
 };
+
+export const shareProfile = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const { platform } = req.query;
+    const deeplink = `thryffy://users/${userId}`;
+
+    const downloadLink = _.cond([
+      [
+        _.curry(_.eq)('android'),
+        _.constant('https://play.google.com/store/apps/details?id=com.thryffy')
+      ],
+      [
+        _.curry(_.eq)('ios'),
+        _.constant('https://apps.apple.com/my/app/thryffy-buy-sell-clothes/id1538541364')
+      ]
+    ])(_.toLower(platform));
+
+    const script = `
+        <script>
+          window.location.href = "${deeplink}";
+          setTimeout(function() {
+            window.location.href = "${downloadLink}";
+          }, 1000);
+        </script>
+      `;
+
+    return res.send(script);
+  } catch (e) {
+    return next(e);
+  }
+};
--- src/routes/mobile/shares.routes.js
+++ src/routes/mobile/shares.routes.js
@@ -1,8 +1,9 @@
 import Router from 'express';
-import { shareProduct } from '@controllers/share.controller';
+import { shareProduct, shareProfile } from '@controllers/share.controller';
 
 const router = new Router();
 
 router.get('/product/:productId', shareProduct);
+router.get('/profile/:userId', shareProfile);
 
 export default router;
